aa3484722bdd1f2c980cbd408b8fd0051a43f07d
Greg Wogan-Browne <gbrowne@marchex.com>
2018-12-19 14:24:10 -0800

    Flattened TreeF

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,5 +1,9 @@
 // A tree of strings where the terminal case has a special annotation that we will care about modeling
 sealed trait Tree
-case class Branch(left: Tree, right: Tree) extends Tree
-case class Node(value: Either[String, Int], repeats: Boolean) extends Tree
+
+case class Branch(left: Tree,
+                  right: Tree) extends Tree
+
+case class Node(value: Either[String, Int],
+                repeats: Boolean) extends Tree
 
