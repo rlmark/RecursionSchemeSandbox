c32f8d89c2a6ea1632ac5c562eaa20397fc0dbb9
rebeccamark <rebecca.mark@47deg.com>
2018-12-19 10:06:15 -0800

    Simplfying working code

--- src/main/scala/TreeF.scala
+++ src/main/scala/TreeF.scala
@@ -1,7 +1,7 @@
 import cats.implicits._
 import cats.{Applicative, Traverse}
+import qq.droste.Coalgebra
 import qq.droste.util.DefaultTraverse
-import qq.droste.{Algebra, Basis, Coalgebra}
 
 sealed trait TreeF[A]
 
@@ -17,17 +17,17 @@ object TreeF {
     new DefaultTraverse[TreeF] {
       def traverse[G[_]: Applicative, A, B](fa: TreeF[A])(f: A => G[B]): G[TreeF[B]] =
         fa match {
-          case c: StringNodeF[B] => (c: TreeF[B]).pure[G]
-          case c: RepeatNodeF[B] => (c: TreeF[B]).pure[G]
+          case StringNodeF(s) => (StringNodeF(s): TreeF[B]).pure[G] // DO THIS.
+          case RepeatNodeF(r) => (RepeatNodeF(r): TreeF[B]).pure[G]
           case BranchF(l, r) => (f(l), f(r)).mapN(BranchF(_, _))
         }
     }
 
-  val embedAlgebra: Algebra[TreeF, Tree] = Algebra {
-    case StringNodeF(v) => Node(v, false)
-    case RepeatNodeF(a) => Node(a, true)
-    case BranchF(l, r) => Branch(l, r)
-  }
+//  val embedAlgebra: Algebra[TreeF, Tree] = Algebra {
+//    case StringNodeF(v) => Node(v, false)
+//    case RepeatNodeF(a) => Node(a, true)
+//    case BranchF(l, r) => Branch(l, r)
+//  }
 
   val fromTree: Coalgebra[TreeF, Tree] = Coalgebra {
     case Branch(l, r) => BranchF(l, r)
